b13efbad2bc30a2a25457c749a82cc6afe79d27f
Chavanlak <chavanlak.p@ku.th>
2021-03-25 17:28:12 +0700

    printf("Not cube free");

---
+++ Cube Free Numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        for(i=1;i<=n;i++)
+            printf("%d\n",i);
+    }
+
+
+
+
+   else
+        printf("Not cube free");
+
+}
