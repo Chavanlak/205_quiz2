d3c5195be6123f567c69d2f6f050b3ca5d8e7740
Chavanlak <chavanlak.p@ku.th>
2021-03-25 18:56:19 +0700

    void cube_freenumber()

--- Cube Free Numbers.c
+++ Cube Free Numbers.c
@@ -1,33 +1,39 @@
 #include<stdio.h>
-    int i,j,n,x,k;
+int k[1000001]={0};
+void cube_freenumber()
+{
+
+    int i,j,n,x;
     for(i=n;i<=n;i++)
     {
-        if(k[i]>0)
+        if(k[i]==0)
         {
            x=i*i*i;
            for(j=x;j<=1000000;j+=x)
            k[j]=-1;
         }
-
-            for(i=1;i<=1000000;i++)
-                if(k[i]=x++)
     }
-    int main()
+        x=1;
+        for(i=1;i<=1000000;i++)
+        if(k[i]=x++)
+}
+
+int main()
     {
         cube_freenumber();
-        int t,y;
+        int i,t,y;
         scanf("%d",&t);
         for(i=1;i<=t;i++)
         {
             scanf("%d",&y);
-            if()
+            if(k[y]=-1)
             {
-
+                printf("%d",k[y]);
             }
             else
-                printf("");
+                printf("Not cube free ");
         }
-
+        return 0;
     }
 
 
@@ -43,4 +49,6 @@
 
 
 
-}
+
+
+
